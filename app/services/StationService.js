c63ced47a246e1bdd93300f88e42d53f81e42cc5
José P. L. Martín <arguser@gmail.com>
2015-11-11 13:47:27 -0300

    Drag map, choose language, more

--- app/services/StationService.js
+++ app/services/StationService.js
@@ -1,5 +1,6 @@
 app.service('StationService', function($filter) {
   this._stations = stations;
+  this._stationNames = station_names;
 
   this.getList = function() {
     return this._stations;
@@ -8,6 +9,17 @@ app.service('StationService', function($filter) {
   this.getById = function(id) {
     var found = $filter('filter')(this._station, {
       station_id: id
+    }, true);
+    if (found.length) {
+      return found[0];
+    } else {
+      return false;
+    }
+  };
+
+  this.getNameByIdAndLocale = function(id, lang) {
+    var found = $filter('filter')(this._stationNames, {
+      station_id: id, name_set: lang
     }, true);
     if (found.length) {
       return found[0];
